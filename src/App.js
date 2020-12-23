09cb6cd7a73131e5e1ee9992bf183e47baf5b99f
Sarvenaz Sarvghad <sarvghad79@gmail.com>
2020-12-23 11:05:51 +0330

    I forgot to save sth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Home from './containers/Home/Home';
-import Toggle from './components/UI/switch/ToggleSwitchComp';
+import Home from './containers/Home/Home';
+// import Toggle from './components/UI/switch/ToggleSwitchComp';
 
 
 const App = () => {
   return (
-    <Toggle />
+    <Home />
   );
 }
 
