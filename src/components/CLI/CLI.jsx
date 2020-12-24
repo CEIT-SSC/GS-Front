18c40d2f103a5345e195b39fd5e1d3995bb78ec5
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2020-12-25 00:41:29 +0330

    file command added

--- src/components/CLI/CLI.jsx
+++ src/components/CLI/CLI.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Terminal from 'react-console-emulator';
 import axios from 'axios';
 
 const CLI = props => {
     const terminal = React.createRef();
+    const inputRef = React.createRef();
+    const [file, setFile] = useState(null);
 
     const commands = {
         jokes: {
@@ -11,29 +13,62 @@ const CLI = props => {
             usage: 'jokes <number>',
             fn: function (arg) {
                 const sendReq = async () => {
-                    const ter = terminal.current;
                     try {
                         const response = await axios.get("http://api.icndb.com/jokes/random/" + arg);
-                        ter.pushToStdout(response.data.value.map(el => el.joke + "\n"));
+                        terminal.current.pushToStdout(response.data.value.map(el => el.joke + "\n"));
                     }
                     catch (err) {
-                        ter.pushToStdout(err.message);
+                        terminal.current.pushToStdout(err.message);
                     }
                 }
                 sendReq();
                 return `${"wait.."}`
             }
+        },
+
+        file: {
+            description: 'uploads a file to the local server',
+            usage: 'file',
+            fn: function (arg) {
+                setFile(null);
+                inputRef.current.click();
+            }
         }
     }
 
+    useEffect(() => {
+        if (file != null) {
+            const data = new FormData()
+            for (let el in file) {
+                data.append('files', file[el]);
+            }
+            axios.post("http://localhost:5000/files", data, { // receive two parameter endpoint url ,form data 
+            })
+                .then(res => { // then print response status
+                    console.log(res.status)
+                    terminal.current.pushToStdout("success");
+                })
+                .catch(err => { console.log(err) })
+        }
+    }, [file])
+
+
+
     return (
-        <Terminal
-            style={{ width: '70%', height: '60%' }}
-            ref={terminal}
-            commands={commands}
-            welcomeMessage={'Welcome to the React terminal!'}
-            promptLabel={'me@React:~$'}
-        />
+        <>
+            <input type="file" multiple
+                style={{ display: 'none' }}
+                ref={inputRef}
+                onChange={(event) => { setFile(event.target.files) }}
+            />
+            <Terminal
+                style={{ width: '70%', height: '60%' }}
+                ref={terminal}
+                commands={commands}
+                welcomeMessage={'Welcome to the React terminal!'}
+                promptLabel={'me@React:~$'}
+            />
+        </>
     );
 }
 
