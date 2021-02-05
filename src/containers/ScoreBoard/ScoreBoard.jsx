b9decd7e667be107cca1f0b22fb20c7a50d64005
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2021-02-06 03:14:50 +0330

    select input for scoreboard

--- src/containers/ScoreBoard/ScoreBoard.jsx
+++ src/containers/ScoreBoard/ScoreBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, connect } from 'react-redux';
-import { Table, Row, Col, Button } from 'react-bootstrap';
+import { Table, Row, Col, Button, Form } from 'react-bootstrap';
 import { MainContainer, ListContainer, BackLink } from './ScoreBoardStyle';
 
 import * as actions from '../../store/actions';
@@ -12,12 +12,6 @@ const ScoreBoard = (props) => {
     const [show99, setShow99] = useState(false);
     const { scores, username, theme } = props;
     let dataList = null;
-
-    const changeScoresHandler = (event) => {
-        const show = !show99;
-        setShow99(show);
-
-    }
 
     if (scores === null) {
         dispatch(actions.getScoreBoard());
@@ -55,9 +49,11 @@ const ScoreBoard = (props) => {
                         <ListContainer isdark={theme === 'DARK'}>
                             <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
                                 <h1> ScoreBoard</h1>
-                                <Button style={{width:'130px', height: '40px'}} onClick={(event) => changeScoresHandler(event)}>
-                                    {show99 ? 'Elders :)' : 'Youngers :)'}
-                                </Button>
+                                <Form.Control style={{width:'100px'}}
+                                as="select" value={show99} onChange={() => setShow99(!show99)}>
+                                    <option value={true}>99s</option>
+                                    <option value={false}>others</option>
+                                </Form.Control>
                             </div>
                             <Table>
                                 <thead>
